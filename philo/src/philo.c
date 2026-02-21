3569e041579491caafd16575f3e31f44acc23f1f
julauren <249057325+Simeriash@users.noreply.github.com>
2026-02-21 17:18:17 +0100

    try to thread

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,11 +6,35 @@
 /*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 12:41:04 by julauren          #+#    #+#             */
-/*   Updated: 2026/02/21 10:47:34 by julauren         ###   ########.fr       */
+/*   Updated: 2026/02/21 17:17:35 by julauren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
+
+static void	*ft_thread_routine(void *arg)
+{
+	t_node	*node;
+
+	node = (t_node *)arg;
+	printf("Thread [%ld] - philo n⁰ %i\n", node->thread, node->val);
+	return (NULL);
+}
+
+static void	ft_thread(t_node *table, t_data *data)
+{
+	int		i;
+	t_node	*node;
+
+	node = table->next;
+	i = 1;
+	while (i <= data->nb_philo)
+	{
+		pthread_create(&node->thread, NULL, &ft_thread_routine, node);
+		node = node->next;
+		i++;
+	}
+}
 
 int	main(int ac, char **av)
 {
@@ -25,5 +49,7 @@ int	main(int ac, char **av)
 	table = ft_everyone_to_the_table(data.nb_philo);
 	if (!table)
 		return (0);
+	ft_thread(table, &data);
+	// thread join pour chaque thread
 	ft_free_table(table);
 }
