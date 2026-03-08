16dc527e239e0bc3ebe18fbd87b6831a223f7e71
julauren <249057325+Simeriash@users.noreply.github.com>
2026-03-08 13:58:46 +0100

    new init condition

--- philo/src/parse.c
+++ philo/src/parse.c
@@ -39,15 +39,15 @@ static int	ft_atoi_philo(const char *str, int *num)
 
 static int	ft_no_time_2_lose(t_data *data)
 {
-	if (data->nb_philo == 0 || data->time_2_die == 0
-		|| (data->time_2_die < (data->time_2_eat + data->time_2_sleep))
-		|| data->time_2_die < data->time_2_eat
-		|| data->time_2_die < data->time_2_sleep
-		|| data->nb_times == 0)
+	if (data->nb_philo == 0 || data->time_2_die == 0 || data->nb_times == 0)
 	{
 		free(data);
 		return (1);
 	}
+	data->end = 1;
+	data->nb_meal = 0;
+	pthread_mutex_init(&data->data, NULL);
+	pthread_mutex_init(&data->printf, NULL);
 	return (0);
 }
 
@@ -66,7 +66,7 @@ int	parse(int ac, char **av, t_data **data)
 	}
 	if (ac == 6)
 	{
-		if(ft_atoi_philo(av[5], &(*data)->nb_times))
+		if (ft_atoi_philo(av[5], &(*data)->nb_times))
 		{
 			free(*data);
 			return (1);
@@ -76,9 +76,5 @@ int	parse(int ac, char **av, t_data **data)
 		(*data)->nb_times = -1;
 	if (ft_no_time_2_lose(*data))
 		return (1);
-	(*data)->end = 1;
-	(*data)->nb_meal = 0;
-	pthread_mutex_init(&(*data)->data, NULL);
-	pthread_mutex_init(&(*data)->printf, NULL);
 	return (0);
 }
